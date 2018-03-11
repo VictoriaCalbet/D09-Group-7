3c03cb1dd77eb8b5b1d3d719735a98a5feddbc19
kain90 <utri1990@gmail.com>
2018-03-11 21:21:10 +0100

    Service - Service
    Implementado el método ServiceService.java

--- Acme-Rendezvous/src/main/java/services/ServiceService.java
+++ Acme-Rendezvous/src/main/java/services/ServiceService.java
@@ -1,26 +1,37 @@
 
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.ServiceRepository;
+import domain.Administrator;
+import domain.Category;
+import domain.Manager;
+import domain.Request;
+import domain.Service;
 
-@Service
+@org.springframework.stereotype.Service
 @Transactional
 public class ServiceService {
 
 	// Managed Repository -----------------------------------------------------
 
 	@Autowired
-	private ServiceRepository	serviceRepository;
-
+	private ServiceRepository		serviceRepository;
 
 	// Supporting services ----------------------------------------------------
+
+	@Autowired
+	private AdministratorService	administratorService;
+
+	@Autowired
+	private ManagerService			managerService;
+
 
 	// Constructors -----------------------------------------------------------
 
@@ -30,39 +41,141 @@ public class ServiceService {
 
 	// Simple CRUD methods ----------------------------------------------------
 
-	public domain.Service create() {
-		return null;
-	}
+	public Service create() {
+		Service result = null;
 
-	public domain.Service save(final domain.Service service) {
-		Assert.notNull(service);
-		domain.Service result;
+		result = new Service();
 
-		result = this.serviceRepository.save(service);
+		result.setCategories(new ArrayList<Category>());
+		result.setRequests(new ArrayList<Request>());
+		result.setIsInappropriate(false);
 
 		return result;
-
 	}
 
-	public domain.Service saveFromCreate(final domain.Service service) {
-		return null;
-	}
-
-	public domain.Service saveFromEdit(final domain.Service service) {
-		return null;
-	}
-
-	// Other business methods -------------------------------------------------
-
-	public Collection<domain.Service> findAll() {
-		Collection<domain.Service> result = null;
+	public Collection<Service> findAll() {
+		Collection<Service> result = null;
 		result = this.serviceRepository.findAll();
 		return result;
 	}
 
-	public domain.Service findOne(final int serviceId) {
-		domain.Service result = null;
+	public Service findOne(final int serviceId) {
+		Service result = null;
 		result = this.serviceRepository.findOne(serviceId);
 		return result;
+	}
+
+	public Service save(final Service service) {
+		Service result = null;
+
+		Assert.notNull(service, "message.error.service.null");
+		result = this.serviceRepository.save(service);
+
+		return result;
+	}
+
+	public Service saveFromCreate(final Service service) {
+		Service result = null;
+		Manager manager = null;
+
+		manager = this.managerService.findByPrincipal();
+
+		Assert.notNull(service, "message.error.service.null");
+		Assert.notNull(manager, "message.error.service.principal.null");
+		Assert.notNull(service.getManager().equals(manager), "message.error.service.userNotPrincipal");
+
+		// Paso 1: realizo la entidad del servicio Service
+
+		result = this.save(service);
+
+		// Paso 2: persisto el resto de relaciones a las que el objeto Service está relacionada
+
+		manager.getServices().add(result);
+		this.saveServiceInCategoriesCollection(result);
+
+		return result;
+	}
+
+	public Service saveFromEdit(final Service service) {
+		Service result = null;
+		Manager manager = null;
+
+		manager = this.managerService.findByPrincipal();
+
+		Assert.notNull(service, "message.error.service.null");
+		Assert.notNull(manager, "message.error.service.principal.null");
+		Assert.notNull(service.getManager().equals(manager), "message.error.service.userNotPrincipal");
+		Assert.isTrue(service.getIsInappropriate(), "message.error.service.cancelled.true");
+
+		// Persisto la entidad del servicio service
+
+		result = this.save(service);
+
+		return result;
+	}
+
+	public void delete(final Service service) {
+		Manager manager = null;
+
+		manager = this.managerService.findByPrincipal();
+
+		Assert.notNull(service, "message.error.service.null");
+		Assert.notNull(manager, "message.error.service.principal.null");
+		Assert.notNull(service.getManager().equals(manager), "message.error.service.userNotPrincipal");
+
+		// Paso 1: actualizamos el resto de relaciones con la entidad Announcement
+
+		service.getManager().getServices().remove(service);
+		this.deleteServiceInCategoriesCollection(service);
+		this.deleteServiceInRequestsCollection(service);
+
+		// Paso 2: borramos el objeto
+
+		this.serviceRepository.delete(service);
+	}
+
+	// Other business methods -------------------------------------------------
+
+	public Service markAsInapropiate(final Service service) {
+		Service result = null;
+		Administrator administrator = null;
+		Boolean isInapropiate = null;
+
+		administrator = this.administratorService.findByPrincipal();
+		Assert.notNull(administrator);
+
+		isInapropiate = true;
+		service.setIsInappropriate(isInapropiate);
+
+		result = this.serviceRepository.save(service);
+
+		return result;
+	}
+
+	public void saveServiceInCategoriesCollection(final Service service) {
+		Collection<Category> categories = null;
+
+		categories = service.getCategories();
+
+		for (final Category category : categories)
+			category.getServices().add(service);
+	}
+
+	public void deleteServiceInCategoriesCollection(final Service service) {
+		Collection<Category> categories = null;
+
+		categories = service.getCategories();
+
+		for (final Category category : categories)
+			category.getServices().remove(service);
+	}
+
+	public void deleteServiceInRequestsCollection(final Service service) {
+		Collection<Request> requests = null;
+
+		requests = service.getRequests();
+
+		for (final Request request : requests)
+			request.setService(null);
 	}
 }
