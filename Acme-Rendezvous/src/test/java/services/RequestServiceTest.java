c8cc93d6544783281f803b08e036667fc0a8dcf7
davidromesp <32366797+davidromesp@users.noreply.github.com>
2018-03-15 04:07:32 +0100

    RequestServiceTest

--- Acme-Rendezvous/src/test/java/services/RequestServiceTest.java
+++ Acme-Rendezvous/src/test/java/services/RequestServiceTest.java
@@ -1,20 +1,17 @@
 
 package services;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import javax.transaction.Transactional;
-import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
+import domain.Rendezvous;
+import domain.Request;
 import domain.Service;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -27,81 +24,48 @@ public class RequestServiceTest extends AbstractTest {
 	// The SUT (Service Under Test) -------------------------------------------
 
 	@Autowired
-	private ServiceService	serviceService;
+	private ServiceService		serviceService;
 
 	@Autowired
-	private RequestService	requestService;
+	private RequestService		requestService;
 
 	@Autowired
-	private CategoryService	categoryService;
+	private RendezvousService	rendezvousService;
 
 
 	// Tests ------------------------------------------------------------------
 
 	/**
 	 * Create and save a new service
-	 * Test 1: Positive case --> expected: service created
-	 * Test 2: Negative case. Wrong VAT pattern.
-	 * Test 3: Negative case. Future birth date.
+	 * Test 1: Positive case --> expected: Request a services Successful
+	 * Test 2: Negative case. Fail requesting a service
 	 */
 
-	@SuppressWarnings("unchecked")
 	@Test
-	public void testCreateServiceDriver() {
-		// principal(manager), name, description, pictureURL, isInappropriate, requests, categories, expected exception
-		final Object[][] testingData = {
+	public void testCreateRequestDriver() {
+		final Object testingData[][] = {
 			{
-				"manager1", "Service name", "Service description", "http://www.myImage.com", false, new ArrayList<String>(), new ArrayList<String>(Arrays.asList("category1")), null
-			}, {
-				"manager1", null, "Service description", "http://www.myImage.com", false, new ArrayList<String>(), new ArrayList<String>(Arrays.asList("category1")), ConstraintViolationException.class
-			}, {
-				"manager1", "Service name", null, "http://www.myImage.com", false, new ArrayList<String>(), new ArrayList<String>(Arrays.asList("category1")), ConstraintViolationException.class
-			}, {
-				"manager1", "Service name", "Service description", "WRONG://www.myImage.com", false, new ArrayList<String>(), new ArrayList<String>(Arrays.asList("category1")), ConstraintViolationException.class
-			}, {
-				"manager1", "Service name", "Service description", "http://www.myImage.com", true, new ArrayList<String>(), new ArrayList<String>(Arrays.asList("category1")), IllegalArgumentException.class
-			}, {
-				"manager1", "Service name", "Service description", "http://www.myImage.com", false, new ArrayList<String>(Arrays.asList("request1")), new ArrayList<String>(Arrays.asList("category1")), IllegalArgumentException.class
-			}, {
-				"manager1", "Service name", "Service description", "http://www.myImage.com", false, new ArrayList<String>(Arrays.asList("request1", "request2")), new ArrayList<String>(Arrays.asList("category1")), IllegalArgumentException.class
-			}, {
-				"manager1", "Service name", "Service description", "http://www.myImage.com", false, new ArrayList<String>(Arrays.asList("request1", "request2", "request3")), new ArrayList<String>(Arrays.asList("category1")), IllegalArgumentException.class
-			}, {
-				"manager1", "Service name", "Service description", "http://www.myImage.com", false, new ArrayList<String>(), new ArrayList<String>(), IllegalArgumentException.class
+				"user1", "86", "80", "76", null
+
 			}
+
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.testCreateServiceTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (boolean) testingData[i][4], (ArrayList<String>) testingData[i][5],
-				(ArrayList<String>) testingData[i][6], (Class<?>) testingData[i][7]);
+			this.testCreateRequestTemplate((String) testingData[i][0], (int) testingData[i][1], (int) testingData[i][2], (int) testingData[i][3], (Class<?>) testingData[i][4]);
 	}
-	protected void testCreateServiceTemplate(final String manager, final String name, final String description, final String pictureURL, final boolean isInappropriate, final ArrayList<String> requests, final ArrayList<String> categories,
-		final Class<?> expectedException) {
-		Class<?> caught = null;
+	protected void testCreateRequestTemplate(final String username, final int requestId, final int rendezvousId, final int serviceId, final Class<?> expectedException) {
+		Class<?> caught;
+		caught = null;
 
 		try {
-			this.authenticate("manager1");
-
-			Service service = null;
-			Service result = null;
-
-			service = this.serviceService.create();
-
-			service.setName(name);
-			service.setDescription(description);
-			service.setPictureURL(pictureURL);
-			service.setIsInappropriate(isInappropriate);
-
-			for (int i = 0; i < requests.size(); i++)
-				service.getRequests().add(this.requestService.findOne(this.getEntityId(requests.get(i))));
-
-			for (int i = 0; i < categories.size(); i++)
-				service.getCategories().add(this.categoryService.findOne(super.getEntityId(categories.get(i))));
-
-			result = this.serviceService.saveFromCreate(service);
-			this.serviceService.flush();
-
-			Assert.notNull(result);
+			this.authenticate(username);
+			final Service service = this.serviceService.findOne(serviceId);
+			final Request request = this.requestService.findOne(requestId);
+			final Rendezvous rendezvous = this.rendezvousService.findOne(rendezvousId);
+			request.setService(service);
+			request.setRendezvous(rendezvous);
+			this.requestService.saveFromCreate(request);
 
 			this.unauthenticate();
 
