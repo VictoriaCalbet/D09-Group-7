41b42e80eef24cba04b2a114b4d2549017b1dfef
kain90 <utri1990@gmail.com>
2018-03-16 04:08:17 +0100

    Repositories - ServiceRepository
    Añadido método findServicesByRendezvousId

--- Acme-Rendezvous/src/main/java/repositories/ServiceRepository.java
+++ Acme-Rendezvous/src/main/java/repositories/ServiceRepository.java
@@ -15,7 +15,13 @@ public interface ServiceRepository extends JpaRepository<Service, Integer> {
 	@Query("select r.service from Request r join r.rendezvous rq join rq.creator s where s.id = ?1")
 	public Collection<Service> findServicesByUserId(int userId);
 
-	//The best-selling services
+	@Query("select req.service from Rendezvous rvs join rvs.requests req where rvs.id = ?1")
+	public Collection<Service> findServicesByRendezvousId(int rendezvousId);
+
+	// Dashboard queries
+
+	// Acme-Rendezvous 2.0 - Requisito 6.2
 	@Query("select s from Service s  order by s.requests.size desc")
 	public Collection<Service> findBestSellingServices();
+
 }
