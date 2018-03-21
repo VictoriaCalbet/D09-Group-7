0e96785c82c06ad688c7ef87461d4b87daf89d07
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-03-21 12:57:20 +0100

    Updated business name in About page.

--- Acme-Rendezvous/src/main/java/controllers/AboutUsController.java
+++ Acme-Rendezvous/src/main/java/controllers/AboutUsController.java
@@ -10,13 +10,20 @@
 
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.SystemConfigurationService;
+
 @Controller
 @RequestMapping("/about-us")
 public class AboutUsController extends AbstractController {
+
+	@Autowired
+	private SystemConfigurationService	systemConfigurationService;
+
 
 	// Constructors -----------------------------------------------------------
 
@@ -29,8 +36,12 @@ public class AboutUsController extends AbstractController {
 	@RequestMapping(value = "/index")
 	public ModelAndView index() {
 		ModelAndView result;
+		String businessName;
+
+		businessName = this.systemConfigurationService.findMain().getBusinessName();
 
 		result = new ModelAndView("about-us/index");
+		result.addObject("businessName", businessName);
 
 		return result;
 	}
--- Acme-Rendezvous/src/main/webapp/views/about-us/index.jsp
+++ Acme-Rendezvous/src/main/webapp/views/about-us/index.jsp
@@ -19,7 +19,7 @@
 
 <spring:message code="aboutUs.group"/>: 007
 <br/>
-<spring:message code="aboutUs.name"/>: Acme-Rendezvous
+<spring:message code="aboutUs.name"/>: <spring:message text="${businessName}"/>
 <br/>
 <spring:message code="aboutUs.address"/>: <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2993.3493465414317!2d-5.987127351096952!3d37.35805735514961!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xcd36f2ab95e92c3b!2sReina+Mercedes+(E.S.I.+Inform%C3%A1tica)!5e0!3m2!1ses!2ses!4v1456428038447" width="600" height="450" style="border:0"></iframe>
 <br/>
