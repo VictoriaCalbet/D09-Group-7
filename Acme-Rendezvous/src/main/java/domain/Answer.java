065896e91778d5c39a192250712f4fcda346047c
kaze <meh@meh.meh>
2018-03-22 07:57:43 +0100

    Discarted changes in indexed querys

--- Acme-Rendezvous/src/main/java/domain/Answer.java
+++ Acme-Rendezvous/src/main/java/domain/Answer.java
@@ -4,9 +4,7 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -16,9 +14,6 @@ import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
 
 @Entity
 @Access(AccessType.PROPERTY)
-@Table(indexes = {
-	@Index(columnList = "user_id, question_id")
-})
 public class Answer extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
